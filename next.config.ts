8f5cbe1ccefd8a4243e64816a0b6bf26b0f63bd0
nazo6 <git@nazo6.dev>
2025-03-05 11:08:25 +0900

    add ci

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const withNextra = nextra({
   // ... Other Nextra config options
 });
 
-// You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
-  // ... Other Next.js config options
+  output: "export",
 });
